280feb043377ed4468c587c63da8aea9029b89b4
Barry Els <barryels@gmail.com>
2016-07-16 00:13:24 +0200

    adding initial support for tracking projects

--- src/server/index.js
+++ src/server/index.js
@@ -8,30 +8,49 @@ var port = process.env.PORT || 9999,
 	endOfLine = require('os').EOL,
 	router = express.Router();
 
+
+function getProjectName(appName, windowTitle) {
+	// TODO add cleverness here, farm out processing to application-specific adapters, so new ones can be added later
+
+	if (appName === "Google Chrome") {
+		return "Internetting";
+	}
+
+	return "";
+}
+
+
 router.use(function (req, res, next) {
 	next();
 });
+
 
 router.get('/', function (req, res) {
 	res.write('Activity Tracker server is running!');
 	res.end();
 });
 
+
 router.post('/activity', function (req, res) {
 	var response = {},
 		now = new Date(),
-		dateFormat = require('dateformat');
+		dateFormat = require('dateformat'),
+		projectName = "";
 
 	var data = req.query;
-	data.a = unescape(data.a);
-	data.w = unescape(data.w);
-	data.p = '[PROJECT NAME WILL GO HERE]';
-	data.t = now.getTime();
+	response.a = data.a;
+	response.w = data.w;
+	response.t = now.getTime();
 
-	var logStream = fs.createWriteStream(process.cwd() + '/src/data/'+ dateFormat(now, 'yyyy-mm-dd') +'.txt', {'flags': 'a'});
-	logStream.end(JSON.stringify(data) + endOfLine);
+	projectName = getProjectName(response.a, response.w);
 
-	res.json(response);
+	if (projectName) {
+		response.p = projectName;
+	}
+
+	var logStream = fs.createWriteStream(process.cwd() + '/src/data/' + dateFormat(now, 'yyyy-mm-dd') + '.txt', {'flags': 'a'});
+	logStream.end(JSON.stringify(response) + endOfLine);
+
 	res.end();
 });
 
@@ -39,6 +58,7 @@ router.post('/activity', function (req, res) {
 router.get('/command/shutdown', function (req, res) {
 	process.exit();
 });
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
