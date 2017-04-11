596cd706d10d5c704e3c802c47059debafd2ed7a
Barry Els <barryels@gmail.com>
2017-04-11 17:21:27 +0200

    minor updates

--- src/client/linux/main.sh
+++ src/client/linux/main.sh
@@ -23,31 +23,29 @@ get_path_to_me() {
 get_active_app_and_window_title() {
 	local WINDOW_TITLE=''
 
-	while [ 1 ]
+	while true
 	do
-	RESULT=$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLASS WM_NAME)
-	RESULT=$(echo $RESULT | sed 's/"//g')
-	IFS=', ' read -r -a array <<< "$RESULT"
+		RESULT=$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) WM_CLASS WM_NAME)
+		RESULT=$(echo $RESULT | sed 's/"//g')
+		IFS=', ' read -r -a array <<< "$RESULT"
 
-	APP_NAME=${array[3]}
-	WINDOW_TITLE="${array[6]}${array[7]}${array[8]}${array[9]}${array[10]}${array[11]}\ ${array[12]}\ ${array[13]}"
+		APP_NAME=${array[3]}
+		WINDOW_TITLE="${array[6]}${array[7]}${array[8]}${array[9]}${array[10]}${array[11]}\ ${array[12]}\ ${array[13]}"
 
-	if [ $(is_screen_locked) -eq 1 ]
-	then
-  		post_activity_data $PROCESS_NAME_SCREENSAVER $WINDOW_TITLE_SCREENSAVER_ACTIVE
-	else
-		post_activity_data $APP_NAME $WINDOW_TITLE
-	fi
+		if [ $(is_screen_locked) -eq 1 ]
+		then
+			post_activity_data $PROCESS_NAME_SCREENSAVER $WINDOW_TITLE_SCREENSAVER_ACTIVE
+		else
+			post_activity_data $APP_NAME $WINDOW_TITLE
+		fi
 
-	sleep ${CONFIG_CLIENT_CHECK_INTERVAL_IN_SECONDS}
-
+		sleep ${CONFIG_CLIENT_CHECK_INTERVAL_IN_SECONDS}
 	done
 }
 
 
 start_indicator(){
-	return 0
-	# $(python $(get_path_to_me)/indicator.py)
+	$(python $(get_path_to_me)/indicator.py)
 }
 
 
@@ -57,14 +55,20 @@ is_screen_locked(){
 
 
 post_activity_data() {
+	$(echo "$1 $2")
 	$(curl -s -X POST http://localhost:$CONFIG_SERVER_PORT/activity?a=$1\&w=$2)
 }
 
 
 startServer() {
-	serverPath="$(get_path_to_me)/../../server/index.js"
-	$(node ${serverPath})
+	# serverPath="$(get_path_to_me)/../../server/index.js"
+	# echo ${serverPath}
+	# $(cd $(get_path_to_me))
+	# $(echo pwd)
+	# $(node ${serverPath})
+	$(node "./src/server/index.js")
 }
 
 
-startServer & start_indicator & get_active_app_and_window_title
+# startServer & start_indicator & get_active_app_and_window_title
+startServer & get_active_app_and_window_title
